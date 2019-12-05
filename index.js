9457d99f071a48f424f9a4a6ad119356de382f9b
unibreakfast <unibreakfast1@gmail.com>
2019-12-05 13:18:25 +0200

    first steps in user route

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const c = console.log
+
 module.exports = async ({method, url, headers}, body, dev=!process.env.PORT)=> {
-  console.log('api imported')
   console.log({method, url, headers, body})
-  return {response: 'from api'}
+
+  url = url.slice(5)
+
+  if (url == 'guest') return [{x: 2, y: 4}, {x: 7, y: 11}]
+
+  if (url == 'user') {
+    const { sid, token } = JSON.parse(headers.session)
+
+    const sessions = [{id: 1, token: 'abc'}, {id: 2, token: 'dfg'}]
+
+    if (sessions.find(s => s.id==sid && s.token==token))
+      return {personal: "info"}
+
+  }
+
+
+  // return {response: 'from api'}
   throw 'no such api'
 }
